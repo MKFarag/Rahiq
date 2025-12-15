e4315e46e2d599c10fa2de5e4ab78f7e81d9d0dd
MKFarag <mohammedkhaled.fa@gmail.com>
2025-12-15 21:00:41 +0200

    Add migration to change the delete behavior of the relationships

---
+++ Infrastructure/Persistence/Migrations/20251215185950_ChangeDeleteBehavior.cs
@@ -0,0 +1,258 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BundleItems_Bundles_BundleId",
+                table: "BundleItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BundleItems_Products_ProductId",
+                table: "BundleItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AspNetUsers_CustomerId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Products_ProductId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Shipping_ShippingId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Types_TypeId",
+                table: "Products");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BundleItems_Bundles_BundleId",
+                table: "BundleItems",
+                column: "BundleId",
+                principalTable: "Bundles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BundleItems_Products_ProductId",
+                table: "BundleItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AspNetUsers_CustomerId",
+                table: "Carts",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Products_ProductId",
+                table: "Carts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Shipping_ShippingId",
+                table: "Orders",
+                column: "ShippingId",
+                principalTable: "Shipping",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Types_TypeId",
+                table: "Products",
+                column: "TypeId",
+                principalTable: "Types",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BundleItems_Bundles_BundleId",
+                table: "BundleItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BundleItems_Products_ProductId",
+                table: "BundleItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_AspNetUsers_CustomerId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Products_ProductId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Shipping_ShippingId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Types_TypeId",
+                table: "Products");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BundleItems_Bundles_BundleId",
+                table: "BundleItems",
+                column: "BundleId",
+                principalTable: "Bundles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BundleItems_Products_ProductId",
+                table: "BundleItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_AspNetUsers_CustomerId",
+                table: "Carts",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Products_ProductId",
+                table: "Carts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Shipping_ShippingId",
+                table: "Orders",
+                column: "ShippingId",
+                principalTable: "Shipping",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Types_TypeId",
+                table: "Products",
+                column: "TypeId",
+                principalTable: "Types",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
