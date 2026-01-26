9231a830153ef7d7af3b7e05470c92e8163b3939
MKFarag <mohammedkhaled.fa@gmail.com>
2026-01-26 13:36:16 +0200

    Add migration of permissions

--- Infrastructure/Persistence/Configurations/RoleClaimConfiguration.cs
+++ Infrastructure/Persistence/Configurations/RoleClaimConfiguration.cs
@@ -6,18 +6,20 @@ internal class RoleClaimConfiguration : IEntityTypeConfiguration<IdentityRoleCla
     {
         // Default data
 
-        //var allPermissions = Permissions.GetAll();
-        //var adminClaims = new List<IdentityRoleClaim<string>>();
-        //var id = 1;
+        var allPermissions = Permissions.GetAll();
+        var adminClaims = new List<IdentityRoleClaim<string>>();
+        var id = 1;
 
         // Admin claims
-        //foreach (var permission in allPermissions)
-        //    adminClaims.Add(new IdentityRoleClaim<string>
-        //    {
-        //        Id = id++,
-        //        RoleId = DefaultRoles.Admin.Id,
-        //        ClaimType = Permissions.Type,
-        //        ClaimValue = permission
-        //    });
+        foreach (var permission in allPermissions)
+            adminClaims.Add(new IdentityRoleClaim<string>
+            {
+                Id = id++,
+                RoleId = DefaultRoles.Admin.Id,
+                ClaimType = Permissions.Type,
+                ClaimValue = permission
+            });
+
+        builder.HasData(adminClaims);
     }
 }
